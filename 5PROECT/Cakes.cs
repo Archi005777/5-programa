c6ca72e4f551425290b0b707fce9c6000b9e2990
Archi005777 <115105146+Archi005777@users.noreply.github.com>
2023-02-05 15:55:35 +0300

    5 programa

---
+++ 5PROECT/Cakes.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5PROECT
+{
+    internal class Cakes
+    {
+        public Cakes(string Osn)
+        {
+            osn = Osn;
+        }
+        public string osn;
+        public List<DopCakes> dops;
+
+    }
+}
